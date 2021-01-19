ce67b6b746c2342dde568b30117fa3f9f1431040
FengShiyi <fengsy@1000video.com.cn>
2021-01-19 21:36:11 +0800

    reflection

--- src/main/java/com/fsy/javasrc/Application.java
+++ src/main/java/com/fsy/javasrc/Application.java
@@ -1,17 +1,17 @@
 package com.fsy.javasrc;
 
+import java.util.Arrays;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import java.util.Arrays;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
 
 /**
  * @author fengsy
@@ -23,34 +23,30 @@ import java.util.concurrent.TimeUnit;
 @EnableScheduling
 public class Application {
 
-    //创建线程池
+    // 创建线程池
     private ExecutorService excutorService =
-                                new ThreadPoolExecutor(4096, 4096,
-                                      0L,TimeUnit.MILLISECONDS,
-                                      new LinkedBlockingQueue<Runnable>());
+        new ThreadPoolExecutor(4096, 4096, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>());
     private int counter;
 
-    //以固定的速率向线程池中加入任务
-//    @Scheduled(fixedRate = 10)
-//    public void lockContention(){
-//        IntStream.range(0,1000000).forEach(i -> excutorService.submit(this::incrementSync));
-//    }
+    // 以固定的速率向线程池中加入任务
+    // @Scheduled(fixedRate = 10)
+    // public void lockContention(){
+    // IntStream.range(0,1000000).forEach(i -> excutorService.submit(this::incrementSync));
+    // }
 
     private synchronized void incrementSync() {
-        counter = (counter+1)%10000000;
+        counter = (counter + 1) % 10000000;
     }
 
-
     public static void main(String[] args) {
+
         SpringApplication.run(Application.class, args);
     }
 
     @Bean
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
-
             System.out.println("Let's inspect the beans provided by Spring Boot:");
-
             String[] beanNames = ctx.getBeanDefinitionNames();
             Arrays.sort(beanNames);
             for (String beanName : beanNames) {
---
+++ src/main/java/com/fsy/javasrc/threads/Cache.java
@@ -0,0 +1,65 @@
+package com.fsy.javasrc.threads;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * @author fengsy
+ * @date 1/15/21
+ * @Description
+ */
+
+class Cache<K, V> {
+    final Map<K, V> m = new HashMap<>();
+    final ReadWriteLock rwl = new ReentrantReadWriteLock();
+    // 读锁
+    final Lock r = rwl.readLock();
+    // 写锁
+    final Lock w = rwl.writeLock();
+
+    // 读缓存
+    V get(K key) {
+        V v = null;
+        r.lock();
+        try {
+            v = m.get(key);
+        } finally {
+            r.unlock();
+        }
+
+        if (v != null) {
+            return v;
+        }
+
+        w.lock();
+        try {
+            v = m.get(key);
+            if (v == null) {
+                v = getFromDB(key);
+                m.put(key, v);
+            }
+        } finally {
+            w.unlock();
+        }
+
+        return v;
+
+    }
+
+    private V getFromDB(K key) {
+        return null;
+    }
+
+    // 写缓存
+    V put(K key, V value) {
+        w.lock();
+        try {
+            return m.put(key, value);
+        } finally {
+            w.unlock();
+        }
+    }
+}
