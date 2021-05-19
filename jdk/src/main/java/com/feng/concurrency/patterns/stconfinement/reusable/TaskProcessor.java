62f5330f31a44d08bdcd90257addd594515c9d46
FengShiyi <fengsy@1000video.com.cn>
2021-05-19 18:16:06 +0800

    concurrency patterns:master slave

---
+++ jdk/src/main/java/com/feng/concurrency/patterns/stconfinement/reusable/TaskProcessor.java
@@ -0,0 +1,18 @@
+package com.feng.concurrency.patterns.stconfinement.reusable;
+
+/**
+ * @author fengsy
+ * @date 5/19/21
+ * @Description
+ */
+public interface TaskProcessor<T, V> {
+    /**
+     * 对指定任务进行处理。
+     *
+     * @param task
+     *            任务
+     * @return 任务处理结果
+     * @throws Exception
+     */
+    V doProcess(T task) throws Exception;
+}
